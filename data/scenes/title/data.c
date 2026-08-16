ad29aa48e9aa6331dcf0600903ec980519c7899e
sawalk <ham.step2@gmail.com>
2026-08-17 05:34:45 +0900

    Update data.c

--- data/scenes/title/data.c
+++ data/scenes/title/data.c
@@ -66,12 +66,12 @@ struct TitleLogoCharData title_logo_char_data[TOTAL_TITLE_LOGO_BUBBLES] = {
     },
     /* Empty */ {
         /* Anim. */ anim_title_logo_goku,
-        /* X, Y  */ 139, 74,
+        /* X, Y  */ 139, 76,
         /* Angle */ -20
     },
     /* ADVANCE */ {
         /* Anim. */ anim_title_logo_advance,
-        /* X, Y  */ 138, 89,
+        /* X, Y  */ 136, 89,
         /* Angle */ 37
     }
 };
