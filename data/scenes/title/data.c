b275fbb3fd6602b3eaefa45883f182d545cf0d05
sawalk <ham.step2@gmail.com>
2026-08-17 10:18:23 +0900

    banjun

--- data/scenes/title/data.c
+++ data/scenes/title/data.c
@@ -41,22 +41,22 @@ struct TitleLogoCharData title_logo_char_data[TOTAL_TITLE_LOGO_BUBBLES] = {
 struct TitleLogoCharData title_logo_char_data[TOTAL_TITLE_LOGO_BUBBLES] = {
     /* RI */ {
         /* Anim. */ anim_title_logo_ri,
-        /* X, Y  */ 62, 79,
+        /* X, Y  */ 62, 89,
         /* Angle */ 84
     },
     /* DUM */ {
         /* Anim. */ anim_title_logo_zu,
-        /* X, Y  */ 100, 89,
+        /* X, Y  */ 100, 79,
         /* Angle */ 192
     },
     /* SE */ {
         /* Anim. */ anim_title_logo_mu,
-        /* X, Y  */ 138, 78,
+        /* X, Y  */ 138, 88,
         /* Angle */ 44
     },
     /* SANG */ {
         /* Anim. */ anim_title_logo_ten,
-        /* X, Y  */ 178, 88,
+        /* X, Y  */ 178, 78,
         /* Angle */ -20
     },
     /* ADVANCE */ {
